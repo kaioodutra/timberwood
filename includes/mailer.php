059d924ff6356002e8b91db2d537c70e0d09945e
Mauricio Maciel <mbmaciel@outlook.com>
2022-04-28 10:31:41 -0300

    Update mailer.php

--- includes/mailer.php
+++ includes/mailer.php
@@ -25,7 +25,7 @@ try {
     $mail->Host       = 'smtp.gmail.com';                       //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
     $mail->Username   = 'comercial@twco.com.br';                //SMTP username
-    $mail->Password   = 'TWcliente7182#';                       //SMTP password
+    $mail->Password   = 'xxxxx';                               //SMTP password
     $SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;               //Enable implicit TLS encryption
     $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
